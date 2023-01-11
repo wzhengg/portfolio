5a50d85b07106bab74185654815ad21cc72fe538
William Zheng <williamzheng2001@gmail.com>
2023-01-10 19:34:03 -0500

    Create skills data

---
+++ src/skills-data.ts
@@ -0,0 +1,59 @@
+const ICON_DIR = '/icons';
+
+export type Skill = {
+  name: string;
+  icons: string[];
+};
+
+export const languagesData: Skill[] = [
+  {
+    name: 'JavaScript/TypeScript',
+    icons: [`${ICON_DIR}/javascript.svg`, `${ICON_DIR}/typescript.svg`],
+  },
+  {
+    name: 'Java',
+    icons: [`${ICON_DIR}/java.svg`],
+  },
+  {
+    name: 'Python',
+    icons: [`${ICON_DIR}/python.svg`],
+  },
+  {
+    name: 'HTML',
+    icons: [`${ICON_DIR}/html.svg`],
+  },
+  {
+    name: 'CSS/SASS',
+    icons: [`${ICON_DIR}/css.svg`, `${ICON_DIR}/sass.svg`],
+  },
+];
+
+export const frameworksData: Skill[] = [
+  {
+    name: 'React.js',
+    icons: [`${ICON_DIR}/react.svg`],
+  },
+  {
+    name: 'Node.js/Express.js',
+    icons: [`${ICON_DIR}/node.svg`, `${ICON_DIR}/express.svg`],
+  },
+  {
+    name: 'MongoDB/Mongoose',
+    icons: [`${ICON_DIR}/mongodb.svg`, `${ICON_DIR}/mongoose.png`],
+  },
+  {
+    name: 'Tailwind CSS',
+    icons: [`${ICON_DIR}/tailwindcss.svg`],
+  },
+];
+
+export const miscellaneousData: Skill[] = [
+  {
+    name: 'Git',
+    icons: [`${ICON_DIR}/git.svg`],
+  },
+  {
+    name: 'Linux',
+    icons: [`${ICON_DIR}/linux.svg`],
+  },
+];
