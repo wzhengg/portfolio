cde8beb951ed51b689ef4a5a10eac37b91216d47
William Zheng <williamzheng2001@gmail.com>
2023-01-10 19:35:33 -0500

    Render skills section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import SkillsSection from './components/SkillsSection';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SkillsSection />
+    </div>
+  );
 }
 
 export default App;
