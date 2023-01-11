166d5e29ea58925954a9cb610d1f397441c65bc5
William Zheng <williamzheng2001@gmail.com>
2023-01-11 12:02:58 -0500

    Render education section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import EducationSection from './components/EducationSection';
 import SkillsSection from './components/SkillsSection';
 
 function App() {
   return (
     <div className="App">
+      <EducationSection />
       <SkillsSection />
     </div>
   );
