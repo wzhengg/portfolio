7a1278ab8f0d6d1681a82abd25214cf267636134
William Zheng <williamzheng2001@gmail.com>
2023-01-11 12:02:51 -0500

    Create education section component

---
+++ src/components/EducationSection.tsx
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+import SectionContainer from './reusable/SectionContainer';
+import SectionHeading from './reusable/SectionHeading';
+
+const EducationContainer = styled(SectionContainer)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const SectionBody = styled.div`
+  @media (min-width: 1024px) {
+    display: flex;
+    gap: 2rem;
+  }
+`;
+
+const WVULogo = styled.img`
+  height: 6rem;
+  margin: 1rem 0;
+
+  @media (min-width: 1024px) {
+    height: 12rem;
+  }
+`;
+
+const SchoolName = styled.h2`
+  font-size: 1.5em;
+  font-weight: 500;
+  color: #f1f5f9;
+  margin: 1rem 0;
+`;
+
+const Detail = styled.p`
+  font-size: 1.2em;
+  color: #e2e8f0;
+  max-width: 480px;
+  padding: 0.5rem 0;
+`;
+
+const Courses = styled.span`
+  font-weight: 300;
+  color: #cbd5e1;
+`;
+
+function EducationSection() {
+  return (
+    <EducationContainer>
+      <SectionHeading name="Education" />
+      <SectionBody>
+        <WVULogo src="/icons/wvu.svg" alt="wvu-logo" />
+        <div>
+          <SchoolName>West Virginia University</SchoolName>
+          <Detail>August 2020 - May 2024</Detail>
+          <Detail>B.S. Computer Science & Computer Engineering</Detail>
+          <Detail>GPA: 4.0</Detail>
+          <Detail>
+            {'Courses: '}
+            <Courses>
+              Data Structures & Algorithms, Operating Systems, Software
+              Engineering, Programming Languages, Artificial Intelligence, Data
+              & Computer Communications, Database Design, Discrete Math
+            </Courses>
+          </Detail>
+        </div>
+      </SectionBody>
+    </EducationContainer>
+  );
+}
+
+export default EducationSection;
