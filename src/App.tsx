6298fc9bdfa58a05a3d4587ce3465e8803038965
William Zheng <williamzheng2001@gmail.com>
2023-01-11 13:29:17 -0500

    Render about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import AboutSection from './components/AboutSection';
 import EducationSection from './components/EducationSection';
 import SkillsSection from './components/SkillsSection';
 
 function App() {
   return (
     <div className="App">
+      <AboutSection />
       <EducationSection />
       <SkillsSection />
     </div>
