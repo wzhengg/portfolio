d3f5a7cd70096706c11913762032eef7636ef8c0
William Zheng <williamzheng2001@gmail.com>
2023-01-11 18:48:23 -0500

    Create contacts data

---
+++ src/contacts-data.ts
@@ -0,0 +1,23 @@
+export type Contact = {
+  name: string;
+  link: string;
+  display: string;
+};
+
+export const contactsData: Contact[] = [
+  {
+    name: 'Email',
+    link: 'mailto: williamzheng2001@gmail.com',
+    display: 'williamzheng2001@gmail.com',
+  },
+  {
+    name: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/wzhengg/',
+    display: 'linkedin.com/in/wzhengg',
+  },
+  {
+    name: 'GitHub',
+    link: 'https://github.com/wzhengg',
+    display: 'github.com/wzhengg',
+  },
+];
