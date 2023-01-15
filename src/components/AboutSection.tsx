bb662f3275e32c56a2a55e2e1b4e4865a531c700
William Zheng <williamzheng2001@gmail.com>
2023-01-14 22:58:17 -0500

    Update about section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -7,13 +7,13 @@ function AboutSection() {
       <Heading name="About Me" />
       <div className="flex items-center justify-around">
         <p className="max-w-xl text-lg text-neutral-100 lg:text-left lg:text-xl">
-          I'm an aspiring software engineer with an interest in technical
-          challenges including complex algorithms and software scalability,
-          architecture, and design. I love learning new programming languages
-          and tech stacks, so I have a hard time sticking to any specific field,
-          but for now, I'm trying my best to get good at full-stack web
-          development. When I'm not coding, you can find me at the gym or
-          reading a book.
+          Hi! 👋 I'm an aspiring software engineer with an interest in the
+          practical application of theory to technical challenges such as
+          software scalability, architecture, and design. I love learning new
+          programming languages and tech stacks, so I have a hard time sticking
+          to any specific field, but for now, I'm trying my best to get good at
+          full-stack web development. When I'm not coding, you can find me at
+          the gym or reading a book.
         </p>
         <p className="hidden text-[12rem] transition-transform hover:scale-110 lg:block">
           👨🏻‍💻
