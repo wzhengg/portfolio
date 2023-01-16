255da54ee535a2b898baad730b4e5f88283b577f
William Zheng <williamzheng2001@gmail.com>
2023-01-16 17:01:25 -0500

    Render projects section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero';
 import AboutSection from './components/AboutSection';
 import EducationSection from './components/EducationSection';
 import SkillsSection from './components/SkillsSection';
+import ProjectsSection from './components/ProjectsSection';
 import ContactSection from './components/ContactSection';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <AboutSection />
       <EducationSection />
       <SkillsSection />
+      <ProjectsSection />
       <ContactSection />
     </div>
   );
